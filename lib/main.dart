80f28dbda8dbcba5c1cf6cd1948df4695edbbe85
thaitani <42672817+thaitani@users.noreply.github.com>
2023-04-14 14:21:56 +0900

    feat(main): flutter error handling 追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
+import 'dart:io';
+
 import 'package:archive_editor/app.dart';
 import 'package:archive_editor/i18n/strings.g.dart';
+import 'package:archive_editor/utils/logger.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
+  FlutterError.onError = (details) {
+    FlutterError.presentError(details);
+    logger.shout(details);
+    if (kReleaseMode) exit(1);
+  };
   WidgetsFlutterBinding.ensureInitialized();
   runApp(
     TranslationProvider(
